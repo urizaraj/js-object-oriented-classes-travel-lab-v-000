16e67a189ce5cc0f46f5e78e9e6db2b4346e0e39
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 20:13:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,7 @@ class Route {
   blocksTravelled() {
     const streetDistance = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
     const avenueDistance = avenues(this.beginningLocation.horizontal, this.endingLocation.horizontal)
+
+    return streetDistance + avenueDistance
   }
 }
