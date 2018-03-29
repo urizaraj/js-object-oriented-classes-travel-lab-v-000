f4a65a059fc862fa7d169fc5abefa1133c1ad2a2
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 19:58:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const month = {
-  'Aug': 8
-}
-
 class Driver {
   constructor(name, date) {
     this.name = name
-
-    const ar = /([a-zA-Z]{3}) (\d{1,2}), (\d{4})/.exec(date)
-
-    // this.startDate = new Date(parseInt(ar[2]), month[ar[0]], parseInt(ar[1]))
     this.startDate = new Date(date)
   }
 }
