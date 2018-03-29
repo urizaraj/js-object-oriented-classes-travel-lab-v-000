346ae2908073c9a5a8ae49861d5f0d7293e0af6c
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 19:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const month = {
+  'Aug': 8
+}
+
 class Driver {
   constructor(name, date) {
     this.name = name
+
+    ar = /([a-zA-Z]{3}) (\d{1,2}), (\d{4})/.exec(date)
+    
     this.date = date
   }
 
-  startDate() {return new Date(this.date)}
+  startDate() {
+    
+  }
 }
