a187d7984ddc1bda1209a2e6dbf7cc6fa560b2cf
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 19:58:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ class Driver {
 
     const ar = /([a-zA-Z]{3}) (\d{1,2}), (\d{4})/.exec(date)
 
-    this.startDate = new Date(parseInt(ar[2]), month[ar[0]], parseInt(ar[1]))
+    // this.startDate = new Date(parseInt(ar[2]), month[ar[0]], parseInt(ar[1]))
+    this.startDate = new Date(date)
   }
 }
