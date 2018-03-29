04bdf0dbd62ede23eaa02893fe98b3c221265d58
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 20:17:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+function avenues(start, end) {
+  i = eastWest.indexOf(start)
+  j = eastWest.indexOf(end)
+
+  return Math.abs(i - j)
+}
+
+class Driver {
+  constructor(name, date) {
+    this.name = name
+    this.startDate = new Date(date)
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    const streetDistance = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    const avenueDistance = avenues(this.beginningLocation.horizontal, this.endingLocation.horizontal)
+
+    return streetDistance + avenueDistance
+  }
+
+  estimatedTime(peak) {
+    const div = (peak ? 2 : 3)
+    return this.blocksTravelled() / div
+  }
+}
